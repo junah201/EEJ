cb8af4449d9c422236974b90d6d8d84126a1b03a
junah201 <junah.dev@gmail.com>
2024-11-25 02:34:44 +0900

    Feat: Add header

---
+++ src/layouts/MainLayout/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header className="flex items-center justify-center w-full h-14 lg:h-16 bg-white border-b border-background">
+      <div className="w-full max-w-screen-md flex justify-between items-center px-2">
+        <span className="text-primary font-bold">
+          Equity, Equality, or Justice: Where Do You Stand?
+        </span>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 
+import Header from "./Header";
+
 const MainLayout = () => {
   return (
     <div className="flex-grow flex flex-col w-full h-full bg-background items-center">
-      <header></header>
+      <Header />
       <main className="flex flex-col flex-grow items-center w-full">
         <div className="w-full h-full max-w-screen-md flex flex-grow flex-col bg-white items-center p-4">
           <Outlet />
