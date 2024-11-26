29640b55351d814721503a4bfeaafa3fb3797c71
junah201 <junah.dev@gmail.com>
2024-11-26 17:46:09 +0900

    Fix: Text layout to left align

--- src/pages/Question.tsx
+++ src/pages/Question.tsx
@@ -11,7 +11,7 @@ const Question = () => {
   if (stage.type === "result") {
     return (
       <>
-        <div className="flex flex-col gap-y-2">
+        <div className="flex flex-col gap-y-2 w-full">
           <h1 className="text-primary font-bold text-lg">{stage.yourType}</h1>
           <p>{stage.result}</p>
           <p>{stage.description}</p>
