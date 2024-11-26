c913808f26a2a9942fff223a9c074febd670b692
junah201 <junah.dev@gmail.com>
2024-11-26 17:44:13 +0900

    Feat: Add logo link to root

--- src/layouts/MainLayout/Header.tsx
+++ src/layouts/MainLayout/Header.tsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
+
+import { ROUTE_MAP } from "@/constants";
+
 const Header = () => {
   return (
     <header className="flex items-center justify-center w-full h-14 lg:h-16 bg-white border-b border-background">
       <div className="w-full max-w-screen-md flex justify-between items-center px-2">
-        <span className="text-primary font-bold">
+        <Link to={ROUTE_MAP.MAIN} className="text-primary font-bold">
           Equity, Equality, or Justice: Where Do You Stand?
-        </span>
+        </Link>
       </div>
     </header>
   );
